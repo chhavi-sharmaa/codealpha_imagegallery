4e3c4e1ea0874c220d1dc1909afd53b84ea7dedc
chhavi-sharmaa <125428739+chhavi-sharmaa@users.noreply.github.com>
2024-06-07 16:55:41 +0530

    Create imagegallery.js

---
+++ imagegallery.js
@@ -0,0 +1,9 @@
+var fullscreen =document.getElementById("fullscreen");
+var fullsc =document.getElementById("fullsc");
+function openfullscreen(pic){
+fullsc.style.display = "flex";
+fullscreen.src = pic;
+}
+function closefullscreen(){
+fullsc.style.display = "none";
+}
